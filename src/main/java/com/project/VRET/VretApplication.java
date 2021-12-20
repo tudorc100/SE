a71babc41f18f7c810faef39df2b26a8534a14eb
tudorc100 <56338313+tudorc100@users.noreply.github.com>
2021-12-20 12:52:13 +0200

    aa
    aaaa

--- src/main/java/com/project/VRET/VretApplication.java
+++ src/main/java/com/project/VRET/VretApplication.java
@@ -1,14 +1,13 @@
-package com.example.project.Controller;
+package com.project.VRET;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectApplication {
-
+public class VretApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
+		SpringApplication.run(VretApplication.class, args);
 	}
 
 }
